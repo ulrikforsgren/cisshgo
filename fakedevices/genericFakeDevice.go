b169fc40d8c1720c04f5d3ff1e748823bd7582d7
Ulrik Stridsman <uforsgre@cisco.com>
2023-01-25 13:01:55 +0100

    Add missing changes for efficient loading of device configuration data files.

--- fakedevices/genericFakeDevice.go
+++ fakedevices/genericFakeDevice.go
@@ -1,11 +1,11 @@
 package fakedevices
 
 import (
-    "fmt"
+	//"fmt"
 	"io/ioutil"
 	"log"
-    "strconv"
-    "strings"
+	"strconv"
+	"strings"
 
 	"github.com/tbotnz/cisshgo/utils"
 )
@@ -15,17 +15,17 @@ import (
 
 // FakeDevice Struct for the device we will be simulating
 type FakeDevice struct {
-    Args              *utils.CmdlineArguments
-	Vendor            string            // Vendor of this fake device
-	Platform          string            // Platform of this fake device
-	Hostname          string            // Hostname of the fake device
-	DefaultHostname   string            // Default Hostname of the fake device (for resetting)
-	Password          string            // Password of the fake device
-	CommandSearch     *utils.MatchCommands // What commands this fake device supports
-	ContextSearch     *utils.MatchContexts // The available CLI prompt/contexts on this fake device
-	ContextHierarchy  *utils.ContextHierarchy // The hierarchy of the available contexts
-    StartingPort      int
-    Port              int
+	Args             *utils.CmdlineArguments
+	Vendor           string                  // Vendor of this fake device
+	Platform         string                  // Platform of this fake device
+	Hostname         string                  // Hostname of the fake device
+	DefaultHostname  string                  // Default Hostname of the fake device (for resetting)
+	Password         string                  // Password of the fake device
+	CommandSearch    *utils.MatchCommands    // What commands this fake device supports
+	ContextSearch    *utils.MatchContexts    // The available CLI prompt/contexts on this fake device
+	ContextHierarchy *utils.ContextHierarchy // The hierarchy of the available contexts
+	StartingPort     int
+	Port             int
 }
 
 // readFile abstracts the standard error handling of opening and reading a file into a string
@@ -39,39 +39,36 @@ func readFile(filename string) string {
 
 // InitGeneric builds a FakeDevice struct for use with cisshgo
 func InitGeneric(
-    args *utils.CmdlineArguments,
+	args *utils.CmdlineArguments,
 	vendor string,
 	platform string,
 	transcript *utils.Transcript,
-    numListeners int,
-    startingPort int,
-    port int,
+	numListeners int,
+	startingPort int,
+	port int,
 ) *FakeDevice {
 
 	// Iterate through the command transcripts and read their contents into our supported commands
 	for _, v := range *transcript.CommandSearch {
-        //fmt.Println(v.File)
-        if v.File.PerDeviceData == true {
-            var name = strings.ReplaceAll(v.File.Name, "<PORT>",strconv.Itoa(port))
-            fmt.Println(name)
-		    v.File.CmdData[port] = readFile(name)
-        }
+		if v.File.PerDeviceData == true {
+			var name = strings.ReplaceAll(v.File.Name, "<PORT>", strconv.Itoa(port))
+			v.File.CmdData[port] = readFile(name)
+		}
 	}
-
 
 	// Create our fake device and return it
 	myFakeDevice := FakeDevice{
-        Args:              args,
-		Vendor:            transcript.Vendor,
-		Platform:          "Undefined", // Currently not used...
-		Hostname:          transcript.Hostname,
-		DefaultHostname:   transcript.Hostname,
-		Password:          transcript.Password,
-		CommandSearch:     transcript.CommandSearch,
-		ContextSearch:     transcript.ContextSearch,
-		ContextHierarchy:  transcript.ContextHierarchy,
-        StartingPort:      startingPort,
-        Port:              port,
+		Args:             args,
+		Vendor:           transcript.Vendor,
+		Platform:         "Undefined", // Currently not used...
+		Hostname:         transcript.Hostname,
+		DefaultHostname:  transcript.Hostname,
+		Password:         transcript.Password,
+		CommandSearch:    transcript.CommandSearch,
+		ContextSearch:    transcript.ContextSearch,
+		ContextHierarchy: transcript.ContextHierarchy,
+		StartingPort:     startingPort,
+		Port:             port,
 	}
 
 	//fmt.Printf("\n%+v\n", myFakeDevice)
--- utils/cmdmatch.go
+++ utils/cmdmatch.go
@@ -2,43 +2,61 @@ package utils
 
 import (
 	// "fmt"
-    "regexp"
+	"io/ioutil"
+	"log"
+	"regexp"
 	"strings"
 )
 
 type CommandFile struct {
-    Name string
-    PerDeviceData bool
-    CmdData map[int]string
+	Name          string
+	PerDeviceData bool
+	CmdData       map[int]string
 }
 
 type CommandPattern struct {
-    Pattern []interface{}
-    File CommandFile
+	Pattern []interface{}
+	File    CommandFile
 }
 
 type MatchCommands []*CommandPattern
 
-func CompileCommands(supportedCommands map[string]string) (*MatchCommands, error) {
-    fieldsMap := make(MatchCommands, 0)
-
-	for cmd, file := range supportedCommands {
-        fields := strings.Fields(strings.ToLower(cmd))
-        comp_fields := make([]interface{}, len(fields))
-        for n,f := range fields {
-            if  strings.HasPrefix(f, "<r>") {
-                comp_fields[n] =regexp.MustCompile("^"+f[3:]+"$")
-            } else {
-                comp_fields[n] = f
-            }
-        }
-        fieldsMap = append(fieldsMap, &CommandPattern{Pattern: comp_fields,
-                           File: CommandFile{Name: file}})
+// readFile abstracts the standard error handling of opening and reading a file into a string
+func readFile(filename string) string {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-    return &fieldsMap, nil
+	return string(content)
 }
 
+func CompileCommands(supportedCommands map[string]string) (*MatchCommands, error) {
+	fieldsMap := make(MatchCommands, 0)
+
+	for cmd, file := range supportedCommands {
+		fields := strings.Fields(strings.ToLower(cmd))
+		comp_fields := make([]interface{}, len(fields))
+		for n, f := range fields {
+			if strings.HasPrefix(f, "<r>") {
+				comp_fields[n] = regexp.MustCompile("^" + f[3:] + "$")
+			} else {
+				comp_fields[n] = f
+			}
+		}
+		// TODO: Improve the perDeviceData mechanism. Far from perfect...
+		cmdData := map[int]string{}
+		perDeviceData := false
+		if strings.Contains(file, "<PORT>") {
+			perDeviceData = true
+		} else {
+			cmdData[0] = readFile(file)
+		}
+		fieldsMap = append(fieldsMap, &CommandPattern{Pattern: comp_fields,
+			File: CommandFile{Name: file, PerDeviceData: perDeviceData, CmdData: cmdData}})
+	}
+
+	return &fieldsMap, nil
+}
 
 // CmdMatch searches the provided supportedCommands to find a match for the provided userInput
 // Returns:
@@ -47,25 +65,25 @@ func CompileCommands(supportedCommands map[string]string) (*MatchCommands, error
 //  multipleMatches: bool
 //	error
 // func CmdMatch(userInput string, supportedCommands *MatchCommands) (bool, string, bool, error) {
-// 
+//
 // 	// Setup our return variables
 // 	match := false
 // 	matchedCmd := ""
 // 	multipleMatches := false
-// 
+//
 // 	// Setup a Map to hold any possibleMatches as keys, and the string.Fields as values
 // 	possibleMatches := make(map[string][]string)
-// 
+//
 // 	// Turn our input string into fields
 // 	// fmt.Printf("userInput: %s\n", userInput)
 // 	userInput = strings.ToLower(userInput) // Lowercase the user input
 // 	userInputFields := strings.Fields(userInput)
-// 
+//
 // 	// Iterate through all the commands in the supportedCommands map
 // 	for supportedCommand := range supportedCommands {
 // 		supportedCommand := strings.ToLower(supportedCommand) // Lowercase our supported command
 // 		commandFields := strings.Fields(supportedCommand)
-// 
+//
 // 		// Match against the 1st field in each command,
 // 		// and that the number of fields is the same,
 // 		// to find any possibleMatches.
@@ -75,26 +93,26 @@ func CompileCommands(supportedCommands map[string]string) (*MatchCommands, error
 // 			possibleMatches[supportedCommand] = commandFields
 // 		}
 // 	}
-// 
+//
 // 	// Setup a map to hold our closestMatch(es)
 // 	closestMatch := make(map[string]struct{})
-// 
+//
 // 	// Iterate through all possibleMatches to find the best match
 // 	// fmt.Printf("possibleMatches: %+v\n", possibleMatches)
 // 	for possibleMatch := range possibleMatches {
-// 
+//
 // 		// First evaluate if we have an exact string match and break/return that
 // 		if userInput == possibleMatch {
 // 			closestMatch[possibleMatch] = struct{}{}
 // 			break
 // 		}
-// 
+//
 // 		// Next, test if the entire input is contained within one of our commands
 // 		if strings.HasPrefix(possibleMatch, userInput) {
 // 			closestMatch[possibleMatch] = struct{}{}
 // 			break
 // 		}
-// 
+//
 // 		// Next delve into the fields and find best match
 // 		for p, possibleMatchField := range possibleMatches[possibleMatch] {
 // 			// fmt.Printf("possibleMatchField: %s\n", possibleMatchField)
@@ -109,7 +127,7 @@ func CompileCommands(supportedCommands map[string]string) (*MatchCommands, error
 // 			}
 // 		}
 // 	}
-// 
+//
 // 	// Evaluate our closestMatch(es)
 // 	if len(closestMatch) > 1 {
 // 		// We had more than two matches to all conditions, return no match!
@@ -127,25 +145,24 @@ func CompileCommands(supportedCommands map[string]string) (*MatchCommands, error
 // 		for k := range closestMatch {
 // 			matchedCmd = k
 // 		}
-// 
+//
 // 	}
-// 
+//
 // 	// fmt.Printf("matchedCmd: %s\n\n", matchedCmd)
 // 	return match, matchedCmd, multipleMatches, nil
 // }
 
-
-
 // CommandMatch searches the provided supportedContexts to find a match for the provided userInput
 // Returns:
+//
 //	match: bool
-// 	matchedCommand: string
+//	matchedCommand: string
 //	error
 func CommandMatch(userInput string, supportedCommands *MatchCommands) (bool, *CommandFile, bool, error) {
 
 	// Setup our return variables
 	match := false
-    var matchedCommand *CommandFile
+	var matchedCommand *CommandFile
 
 	// Turn our input string into fields
 	// fmt.Printf("userInput: %s\n", userInput)
@@ -153,161 +170,156 @@ func CommandMatch(userInput string, supportedCommands *MatchCommands) (bool, *Co
 	userInputFields := strings.Fields(userInput)
 
 	// Iterate through all the commands in the supportedContexts map and create
-    // regexp.
+	// regexp.
 	for _, cmd := range *supportedCommands {
-	    file := cmd.File
-        contextFields := cmd.Pattern
-        if len(userInputFields) == len(contextFields) {
-	        match = true
-            fieldsLoop: for n,f := range contextFields {
-                switch f.(type) {
-                case string:
-                    //fmt.Println("COMP string", f, userInputFields[n])
-                    if !strings.HasPrefix(f.(string), userInputFields[n]) {
-                        match = false
-                        //fmt.Println("NO MATCH!")
-                        break fieldsLoop
-                    }
-                    //fmt.Println("MATCH!")
-                default: // *regexp.Regexp
-                    //fmt.Println("COMP regexp", f, userInputFields[n])
-                    if f.(*regexp.Regexp).MatchString(userInputFields[n]) == false {
-                        match = false
-                        //fmt.Println("NO MATCH!")
-                        break fieldsLoop
-                    }
-                    //fmt.Println("MATCH!")
-                }
-            }
-            if match {
-                matchedCommand = &file
-                break
-            }
-        }
+		file := cmd.File
+		contextFields := cmd.Pattern
+		if len(userInputFields) == len(contextFields) {
+			match = true
+		fieldsLoop:
+			for n, f := range contextFields {
+				switch f.(type) {
+				case string:
+					//fmt.Println("COMP string", f, userInputFields[n])
+					if !strings.HasPrefix(f.(string), userInputFields[n]) {
+						match = false
+						//fmt.Println("NO MATCH!")
+						break fieldsLoop
+					}
+					//fmt.Println("MATCH!")
+				default: // *regexp.Regexp
+					//fmt.Println("COMP regexp", f, userInputFields[n])
+					if f.(*regexp.Regexp).MatchString(userInputFields[n]) == false {
+						match = false
+						//fmt.Println("NO MATCH!")
+						break fieldsLoop
+					}
+					//fmt.Println("MATCH!")
+				}
+			}
+			if match {
+				matchedCommand = &file
+				break
+			}
+		}
 	}
 
 	return match, matchedCommand, false, nil
 }
 
-
-
-type Pattern interface{
-    Match(string)(int)
+type Pattern interface {
+	Match(string) int
 }
-
 
 type StringMatch struct {
-    s string
+	s string
 }
 
-func (s StringMatch)Match(cmd string)(int) {
-    // fmt.Print("StringMatch")
-    if strings.HasPrefix(s.s, cmd) {
-        // fmt.Println(" -> MATCH")
-        return 1
-    }
-    // fmt.Println(" -> NOMATCH")
-    return 0
+func (s StringMatch) Match(cmd string) int {
+	// fmt.Print("StringMatch")
+	if strings.HasPrefix(s.s, cmd) {
+		// fmt.Println(" -> MATCH")
+		return 1
+	}
+	// fmt.Println(" -> NOMATCH")
+	return 0
 }
 
 type RegexpMatch struct {
-    r *regexp.Regexp
+	r *regexp.Regexp
 }
 
-func (r RegexpMatch)Match(cmd string)(int) {
-    // fmt.Print("RegexpMatch")
-    if r.r.MatchString(cmd) {
-        // fmt.Println(" -> MATCH")
-        return 1
-    }
-    // fmt.Println(" -> NOMATCH")
-    return 0
+func (r RegexpMatch) Match(cmd string) int {
+	// fmt.Print("RegexpMatch")
+	if r.r.MatchString(cmd) {
+		// fmt.Println(" -> MATCH")
+		return 1
+	}
+	// fmt.Println(" -> NOMATCH")
+	return 0
 }
 
 type JoinKeyMatch struct {
-    s string
-    r *regexp.Regexp
+	s string
+	r *regexp.Regexp
 }
 
-func (j JoinKeyMatch)Match(cmd string)(int) {
-    // fmt.Print("JoinKeyMatch")
-    if strings.HasPrefix(j.s, cmd) {
-        // fmt.Println(" -> s MATCH")
-        return 1
-    }
-    if j.r.MatchString(cmd) {
-        // fmt.Println(" -> r MATCH")
-        return 2
-    }
-    // fmt.Println(" -> NOMATCH")
-    return 0
+func (j JoinKeyMatch) Match(cmd string) int {
+	// fmt.Print("JoinKeyMatch")
+	if strings.HasPrefix(j.s, cmd) {
+		// fmt.Println(" -> s MATCH")
+		return 1
+	}
+	if j.r.MatchString(cmd) {
+		// fmt.Println(" -> r MATCH")
+		return 2
+	}
+	// fmt.Println(" -> NOMATCH")
+	return 0
 }
-
-
 
 type ContextPattern struct {
-    Pattern []Pattern
-    Context *TranscriptMapContext
-    Commands MatchContexts
+	Pattern  []Pattern
+	Context  *TranscriptMapContext
+	Commands MatchContexts
 }
 
 type MatchContexts []*ContextPattern
 
 type ContextHierarchy map[uint]*ContextPattern
 
-
 func CompileMatches(supportedContexts []*TranscriptMapContext) (*MatchContexts, *ContextHierarchy, error) {
-    flatMap := make(MatchContexts, 0)
-    contextHierarchy := make(ContextHierarchy)
+	flatMap := make(MatchContexts, 0)
+	contextHierarchy := make(ContextHierarchy)
 
 	for _, ctx := range supportedContexts {
-        fields := strings.Fields(strings.ToLower(ctx.Cmd))
-        comp_fields := make([]Pattern, len(fields))
-        for n,f := range fields {
-            if strings.HasPrefix(f, "<r>") {
-                comp_fields[n] = RegexpMatch{regexp.MustCompile("^"+f[3:]+"$")}
-            } else if strings.HasPrefix(f, "<j>") {
-                r := fields[n+1]
-                // TODO: Check statement existence + type <r>
-                m := JoinKeyMatch{
-                        s: f[3:],
-                        r: regexp.MustCompile("^"+f[3:]+r[3:]+"$"),
-                }
-                comp_fields[n] = m
-            } else {
-                comp_fields[n] = StringMatch{f}
-            }
-        }
-        flatMap = append(flatMap, &ContextPattern{ Pattern: comp_fields, Context: ctx})
+		fields := strings.Fields(strings.ToLower(ctx.Cmd))
+		comp_fields := make([]Pattern, len(fields))
+		for n, f := range fields {
+			if strings.HasPrefix(f, "<r>") {
+				comp_fields[n] = RegexpMatch{regexp.MustCompile("^" + f[3:] + "$")}
+			} else if strings.HasPrefix(f, "<j>") {
+				r := fields[n+1]
+				// TODO: Check statement existence + type <r>
+				m := JoinKeyMatch{
+					s: f[3:],
+					r: regexp.MustCompile("^" + f[3:] + r[3:] + "$"),
+				}
+				comp_fields[n] = m
+			} else {
+				comp_fields[n] = StringMatch{f}
+			}
+		}
+		flatMap = append(flatMap, &ContextPattern{Pattern: comp_fields, Context: ctx})
 	}
 
-    // Build hierarchy index
-    for _, mode := range flatMap {
-        contextHierarchy[mode.Context.Id] = mode
-    }
+	// Build hierarchy index
+	for _, mode := range flatMap {
+		contextHierarchy[mode.Context.Id] = mode
+	}
 
-    // Build hierarchy
-    for _, mode := range flatMap {
-        if mode.Context.Up != 0 {
-            contextHierarchy[mode.Context.Up].Commands = append(contextHierarchy[mode.Context.Up].Commands, mode)
-        }
-    }
+	// Build hierarchy
+	for _, mode := range flatMap {
+		if mode.Context.Up != 0 {
+			contextHierarchy[mode.Context.Up].Commands = append(contextHierarchy[mode.Context.Up].Commands, mode)
+		}
+	}
 
-    return &flatMap, &contextHierarchy, nil
+	return &flatMap, &contextHierarchy, nil
 }
-
 
 // ContextMatch searches the provided supportedContexts to find a match for the provided userInput
 // Returns:
+//
 //	match: bool
-// 	matchedCommand: string
+//	matchedCommand: string
 //	error
 func ContextMatch(userInput string, supportedContexts *MatchContexts) (bool, *ContextPattern, bool, error) {
 
 	// Setup our return variables
-//    fmt.Println("ContextMatch")
+	//    fmt.Println("ContextMatch")
 	match := false
-    var matchedContext *ContextPattern = nil
+	var matchedContext *ContextPattern = nil
 
 	// Turn our input string into fields
 	// fmt.Printf("userInput: %s\n", userInput)
@@ -315,34 +327,34 @@ func ContextMatch(userInput string, supportedContexts *MatchContexts) (bool, *Co
 	userInputFields := strings.Fields(userInput)
 
 	// Iterate through all the commands in the supportedContexts map and create
-    // regexp.
+	// regexp.
 	for _, cmd := range *supportedContexts {
-        // fmt.Println("Matching command", cmd.Context.Cmd)
-	    supportedContext := cmd
-        contextFields := cmd.Pattern
+		// fmt.Println("Matching command", cmd.Context.Cmd)
+		supportedContext := cmd
+		contextFields := cmd.Pattern
 
-        n_i := 0
-        n_c := 0
-	    match = true
-        for n_i<len(userInputFields) && n_c<len(contextFields) {
-            f := contextFields[n_c]
-            m_c := f.Match(userInputFields[n_i])
-            if m_c == 0 {
-                match = false
-                // fmt.Println("BREAK")
-                break
-            }
-            n_c += m_c
-            n_i += 1
-        }
-        if match {
-            // fmt.Println(n_i, len(userInputFields), n_c, len(contextFields))
-            if n_i==len(userInputFields) && n_c==len(contextFields) {
-                matchedContext = supportedContext
-                break
-            }
-            // fmt.Println("Len mismatch", match)
-        }
+		n_i := 0
+		n_c := 0
+		match = true
+		for n_i < len(userInputFields) && n_c < len(contextFields) {
+			f := contextFields[n_c]
+			m_c := f.Match(userInputFields[n_i])
+			if m_c == 0 {
+				match = false
+				// fmt.Println("BREAK")
+				break
+			}
+			n_c += m_c
+			n_i += 1
+		}
+		if match {
+			// fmt.Println(n_i, len(userInputFields), n_c, len(contextFields))
+			if n_i == len(userInputFields) && n_c == len(contextFields) {
+				matchedContext = supportedContext
+				break
+			}
+			// fmt.Println("Len mismatch", match)
+		}
 	}
 
 	return match, matchedContext, false, nil
